6dc7b962697c2ede4867a5e8f3c8a0352ffe5cc2
Dextr0 <ipspmoderator@gmail.com>
2015-02-13 20:26:09 +0100

    Parte HTML de formulario.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <title>PHP + MySQL</title>
+    
+</head>
+<body>
+<form action="formulario.php" method="POST">
+    Nombre: <input type="text" name="nombre" id="nombre" /> <br />
+    Apellido: <input type="text" name="apellido" id="apellido" /> <br />
+    Edad: <input type="text" name="edad" id="edad" /> <br />
+    
+        <input type="hidden" name="formulario" value="datos"></br>
+    <button type="submit">Enviar Datos</button> </form> <a href="index.php"><button type="submit">Volver Atras</button></a>
+</body>
