0912cebbd24f35d7e64c0bdf27a9d015adbeceee
Dextr0 <ipspmoderator@gmail.com>
2015-02-13 20:22:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <title>PHP + MySQL</title>
+    
+</head>
+<body>
+    <a href="form.php"><button type="submit">Añadir Datos</button></a> <br />
+    <a href="cons.php"> <button type="submit">Consultar y Modificar Datos</button><a> <br />
+<!--    <button type="submit">Borrar Datos</button> <br /> -->
+</body>
