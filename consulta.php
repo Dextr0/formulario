9286b1375a560cd690dd360b964cc979f6c09bb7
Dextr0 <ipspmoderator@gmail.com>
2015-02-16 12:33:20 +0100

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -6,9 +6,9 @@ $sql = "SELECT * FROM datos WHERE nombre='$_POST[nombre]'";
 
 $res=mysql_query($sql);
 while ($fila=mysql_fetch_assoc($res))
-
 {
-    echo " $fila[nombre] $fila[apellido] $fila[edad] <a href=modificar.php?id=$fila[id]> modificar</a><br/>";
+    echo " $fila[nombre] $fila[apellido] $fila[edad] <a href=modificar.php?Id=$fila[Id]> Modificar</a> - <a href=borrabd.php?Id=$fila[Id]> Borrar</a><br/>";
 }
 
 ?>
+<a href="cons.php"><button type="submit">Volver Atras</button></a>
